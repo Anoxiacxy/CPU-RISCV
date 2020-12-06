38ee3001a9bbfe4d685b5be6a672b9611c908655
Anoxiacxy <anoxiacxy@AnoxiacxydeiMac.local>
2020-12-06 21:36:01 +0800

    clear

--- riscv/src/icache.v
+++
@@ -1,7 +0,0 @@
-module icache (    
-    input wire                  request,
-    input wire [`MemAddrBus]    inst_addr,
-    output reg                  inst_done,
-    output reg [`MemDataBus]    inst_data,
-);
-endmodule
